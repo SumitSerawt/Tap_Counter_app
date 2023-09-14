b705dd377fcc9e31072b11a335dee6e7ef25ece1
Sumit <ss4366@njit.edu>
2023-09-14 17:01:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/tap_counter_app/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.tap_counter_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    var counter = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<ImageButton>(R.id.button)
+        val text = findViewById<TextView>(R.id.textView)
+
+        val upgradeButton = findViewById<Button>(R.id.button2)
+
+        button.setOnClickListener{
+
+            counter++
+            text.text = counter.toString()
+
+            if (counter >= 100){
+                upgradeButton.visibility = View.VISIBLE
+
+                upgradeButton.setOnClickListener {
+//                    button.text = "Add 2"
+
+                    button.setOnClickListener {
+
+                        counter += 2
+                        text.text = counter.toString()
+                    }
+
+                    upgradeButton.visibility = View.INVISIBLE
+
+                }
+
+            }
+        }
+    }
+}
